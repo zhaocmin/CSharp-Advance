f6157ec955af87d18a66e755634637463bb003ab
changmin1001 <zhangna93@hotmail.com>
2020-05-26 14:15:59 -0400

    Assignment2

---
+++ AssignmentAdv2/AssignmentAdv2/ChangMinZhaoClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentAdv2
+{
+    //public interface isString
+    //{
+    //    string value { get; }
+    //}
+    class ChangMinZhaoClass<T>
+    {
+        private T name;
+        public ChangMinZhaoClass(T value)
+        {
+            name = value;
+        }
+        public T print()
+        {
+            return name;
+        }
+    }
+}
---
+++ AssignmentAdv2/AssignmentAdv2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AssignmentAdv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChangMinZhaoClass<string> myName = new ChangMinZhaoClass<string>("Changmin Zhao");
+            Console.WriteLine($"Your name is {myName.print()}");
+        }
+    }
+}
